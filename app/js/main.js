976012c18f84727226894a1e6c6ff71b101634bb
Jesse Soldat <jsoldat@hotmail.com>
2015-11-10 12:01:25 -0500

    angular nav

--- app/js/main.js
+++ app/js/main.js
@@ -9,16 +9,19 @@ var config = function config($stateProvider, $urlRouterProvider) {
 
   $urlRouterProvider.otherwise('/');
 
-  $stateProvider.state('home', {
+  $stateProvider.state('root', {
+    abstract: true,
+    templateUrl: 'template/layout.tpl.html'
+  }).state('root.home', {
     url: '/',
     controller: 'HomeController',
     templateUrl: 'template/home.tpl.html'
-  }).state('about', {
+  }).state('root.about', {
     url: '/about',
     controller: 'HomeController',
     templateUrl: 'template/about.tpl.html'
 
-  }).state('contact', {
+  }).state('root.contact', {
     url: '/contact',
     controller: 'HomeController',
     templateUrl: 'template/contact.tpl.html'
--- js/config.js
+++ js/config.js
@@ -4,18 +4,22 @@ let config = function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/');
 
   $stateProvider 
-    .state('home', {
+    .state('root', {
+      abstract: true,
+      templateUrl: 'template/layout.tpl.html'
+    })
+    .state('root.home', {
       url: '/',
       controller: 'HomeController',
       templateUrl: 'template/home.tpl.html'
     })
-    .state('about', {
+    .state('root.about', {
       url: '/about',
       controller: 'HomeController',
       templateUrl: 'template/about.tpl.html'
 
     })
-    .state('contact', {
+    .state('root.contact', {
       url: '/contact',
       controller: 'HomeController',
       templateUrl: 'template/contact.tpl.html'
